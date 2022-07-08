97ba936e047cf9c5ab1c02c36d062eaf6425cc55
Gerald Joseph <geraldjosephk@gmail.com>
2022-07-08 20:01:08 +0300

    single listing page and fixing bugs

--- listings/urls.py
+++ listings/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'listings'
 urlpatterns = [
-    path('', views.index, name="listings"),
+    path('', views.index, name="index"),
     path('<int:listing_id>', views.listing, name="listing"),
     path('search/', views.search, name="search"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -18,7 +18,11 @@ def index(request):
 
 def listing(request,listing_id):
     """Listing Page"""
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     """Search functionality"""
