4197f2515341a0cd8b9ada3f6662b3a1fb2a5cac
Gerald Joseph <geraldjosephk@gmail.com>
2022-07-24 07:27:43 +0300

    User registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     """Register user"""
     if request.method == 'POST':
-        messages.error(request,'Testing error message')
-        print('Registered')
-        return redirect('accounts:register')
-    else:
-        return render(request,'accounts/register.html')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,'Username already used')
+                return redirect('accounts:register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request,'Email already used')
+                return redirect('accounts:register')
+            else:
+                user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+                user.save()
+                messages.success(request,'Registration successful. You can login')
+                return redirect('accounts:login')
+
+        else:
+            messages.error(request,'Passwords do not match')
+            return redirect('accounts:register')
+        
+    return render(request,'accounts/register.html')
 
 def login(request):
     """Login user"""
