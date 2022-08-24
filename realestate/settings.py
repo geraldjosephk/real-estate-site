29f32d394ba93314e44ed2c2056fd7648ab07212
Gerald Joseph <geraldjosephk@gmail.com>
2022-08-24 18:46:58 +0300

    Inqury check and send email to realtor

--- realestate/settings.py
+++ realestate/settings.py
@@ -148,3 +148,10 @@ MESSAGE_TAGS = {
     messages.SUCCESS: 'success',
     
 }
+
+#Email config
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ' '
+# EMAIL_USE_TLS = True
